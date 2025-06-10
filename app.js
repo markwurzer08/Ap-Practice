6808665eab833204ff47eab905dc03cb36605df4
markwurzer08 <markwurzer08@gmail.com>
2025-06-10 19:31:41 -0400

    Update app.js

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ const problems = {
       question: "A tube contains a volume of water and a volume of mercury as shown in the figure above. Both ends of the tube are open. Points A and B are at the same level and points C and D are at the same level. The distance between points A and B and the surface of the liquids are shown. Which of the following correctly relates the gauge pressures at the points shown? The density of the water is 1,000 kg/m³ and the density of the mercury is 13,600 kg/m³.",
       image: "https://i.imgur.com/J2e6UnT.png",
       options: [
-        { url: "https://i.imgur.com/1t7rW3e.png", label: "A. Pₐ > Pᵦ and P꜀ > P𝒟" },
-        { url: "https://i.imgur.com/2u8sX4f.png", label: "B. Pₐ > Pᵦ and P꜀ = P𝒟" },
-        { url: "https://i.imgur.com/3v9tY5g.png", label: "C. Pₐ = Pᵦ and P꜀ = P𝒟" },
-        { url: "https://i.imgur.com/4w0uZ6h.png", label: "D. Pₐ = Pᵦ and P꜀ > P𝒟" }
+        "A. Pₐ > Pᵦ and P꜀ > P𝒟" ,
+        "B. Pₐ > Pᵦ and P꜀ = P𝒟" ,
+        "C. Pₐ = Pᵦ and P꜀ = P𝒟" ,
+       "D. Pₐ = Pᵦ and P꜀ > P𝒟" 
       ],
       answer: "B. Pₐ > Pᵦ and P꜀ = P𝒟"
     },
